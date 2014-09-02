47c92f2d3daf55d6b8d095005460ff45064766f5
Jeff Xiong <gigix1980@gmail.com>
2014-09-02 11:34:55 +0300

    organise organisations in a tree

--- app/organisation.js
+++ app/organisation.js
@@ -1,13 +1,49 @@
-var Organisation = function(item) {
+var Organisation = function (item) {
     var jsonContent = JSON.parse(item['atom:content']['#']);
     this.globalId = jsonContent.identifier[0].value;
     this.name = jsonContent.name;
+    this.parentId = getParentId();
+
+    this.addChild = function (child) {
+        if (!this.children) {
+            this.children = [];
+        }
+        this.children.push(child);
+        child.parent = this;
+    };
+
+    function getParentId() {
+        return jsonContent.partOf ? jsonContent.partOf.reference : null;
+    }
 };
 
-Organisation.loadAll = function(url) {
+Organisation.loadAll = function (url) {
     var FeedReader = require(__dirname + '/feed-reader');
     var feedReader = new FeedReader(Organisation, url);
-    return feedReader.loadAll();
+
+    function findOrganisation(allOrganisations, globalId) {
+        return allOrganisations.filter(function (organisation) {
+            return organisation.globalId == globalId;
+        })[0];
+    }
+
+    return feedReader.loadAll().then(function (allOrganisations) {
+        allOrganisations.forEach(function (organisation) {
+            if (organisation.parentId == null) {
+                return;
+            }
+            var parentOrganisation = findOrganisation(allOrganisations, organisation.parentId);
+            parentOrganisation.addChild(organisation);
+        });
+        return allOrganisations;
+    });
+};
+
+Organisation.tree = function (allOrganisations) {
+    var roots = allOrganisations.filter(function (organisation) {
+        return organisation.parentId == null;
+    });
+    return roots[0];
 };
 
 module.exports = Organisation;
--- test/unit/organisation-spec.js
+++ test/unit/organisation-spec.js
@@ -1,10 +1,11 @@
 var Organisation = require(__dirname + '/../../app/organisation');
 
-describe('Organisation', function() {
-    describe('loadAll', function() {
-        it('loads all organisations from a remote end point', function(done) {
-            var endPointUrl = 'http://liberia-staging.mhero.org:8984/CSD/csr/merged_sierra_leone/careServicesRequest/urn:openhie.org:openinfoman-fhir:fhir_organization_read/adapter/fhir/Organization/_search?_format=json&name=&partOf.reference=';
-            Organisation.loadAll(endPointUrl).then(function(allOrganisations) {
+describe('Organisation', function () {
+    var endPointUrl = 'http://liberia-staging.mhero.org:8984/CSD/csr/merged_sierra_leone/careServicesRequest/urn:openhie.org:openinfoman-fhir:fhir_organization_read/adapter/fhir/Organization/_search?_format=json&name=&partOf.reference=';
+
+    describe('loadAll', function () {
+        it('loads all organisations from a remote end point', function (done) {
+            Organisation.loadAll(endPointUrl).then(function (allOrganisations) {
                 expect(allOrganisations.length).toBe(168);
 
                 var firstOrganisation = allOrganisations[0];
@@ -15,6 +16,27 @@ describe('Organisation', function() {
             });
         });
 
-        it('organises all organisations in a hierarchical manner');
+        it('assumes there is only one root', function (done) {
+            Organisation.loadAll(endPointUrl).then(function (allOrganisations) {
+                var roots = allOrganisations.filter(function (organisation) {
+                    return organisation.parentId == null;
+                });
+                expect(roots.length).toBe(1);
+                expect(roots[0].globalId).toBe('urn:dhis.org:sierra-leone-demo:csd:organization:ImspTQPwCqd');
+                done();
+            });
+        });
     });
+
+    describe('tree', function () {
+        it('organises all organisations in a hierarchical manner', function (done) {
+            Organisation.loadAll(endPointUrl).then(function (allOrganisations) {
+                var root = Organisation.tree(allOrganisations);
+                expect(root.globalId).toBe('urn:dhis.org:sierra-leone-demo:csd:organization:ImspTQPwCqd');
+                expect(root.children.length).toBe(13);
+                expect(root.children[0].parent).toBe(root);
+                done();
+            });
+        });
+    })
 });
