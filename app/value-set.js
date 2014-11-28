ce01223e213ea03dce9cf3078c2089dc623246f7
Carl Leitner <litlfred@gmail.com>
2014-11-28 03:24:35 +0000

    cache value set responses

--- app/value-set.js
+++ app/value-set.js
@@ -10,25 +10,37 @@ var ValueSet = function (concepts) {
     };
 };
 
+var cache = {};
+
 ValueSet.load = function (endPoint, codingSystem) {
     var XmlFeedReader = require(__dirname + '/xml-feed-reader');
-    var reader = new XmlFeedReader(endPoint + '?ID=' + codingSystem);
+    var full_endpoint = endPoint + '?ID=' + codingSystem;
+    if (full_endpoint in cache) {
+	return cache[full_endpoint];
+    } else {
+	var reader = new XmlFeedReader(full_endpoint);
 
-    var createValueSet = function(result) {
-        var rawValueSet = result['svs:RetrieveValueSetResponse']['svs:ValueSet'];
-        if(!rawValueSet) {
-            throw 'invalid';
-        }
+	var createValueSet = function(result) {
+            var rawValueSet = result['svs:RetrieveValueSetResponse']['svs:ValueSet'];
+            if(!rawValueSet) {
+		throw 'invalid';
+            }
 
-        var rawConcepts = rawValueSet[0]['svs:ConceptList'][0]['svs:Concept'];
-        var allConcepts = rawConcepts.map(function (rawConcept) {
-            return rawConcept['$'];
-        });
-        return new ValueSet(allConcepts);
-    };
-    var fallback = new ValueSet();
-
-    return reader.load(createValueSet, fallback);
+            var rawConcepts = rawValueSet[0]['svs:ConceptList'][0]['svs:Concept'];
+            var allConcepts = rawConcepts.map(function (rawConcept) {
+		return rawConcept['$'];
+            });
+            return new ValueSet(allConcepts);
+	};
+	var fallback = new ValueSet();
+	var res = reader.load(createValueSet, fallback);
+	if (res) {
+	    cache[full_endpoint] = res;
+	    return res;
+	} else {
+	    return false;
+	}
+    }
 };
 
 module.exports = ValueSet;
